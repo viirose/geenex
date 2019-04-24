2575cfa5fdec734e5a133cb28984d2942f51d0fd
kris <kris@viirose.com>
2019-04-24 20:57:09 +0800

    init
    init

--- resources/views/conf/brands.blade.php
+++ resources/views/conf/brands.blade.php
@@ -26,7 +26,6 @@
       <div class="modal-content">
    
         <div class="modal-header">
-          <h4 class="modal-title">Type manager</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
    
--- resources/views/conf/categories.blade.php
+++ resources/views/conf/categories.blade.php
@@ -48,7 +48,6 @@
       <div class="modal-content">
    
         <div class="modal-header">
-          <h4 class="modal-title">Type manager</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
    

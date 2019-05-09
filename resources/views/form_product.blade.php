8aeed567df981cbe2240cf68f96051627bcee2fd
kris <kris@viirose.com>
2019-05-09 11:57:38 +0800

    init
    init

--- resources/views/form_product.blade.php
+++ resources/views/form_product.blade.php
@@ -10,7 +10,7 @@
 <div class="row top-pad"></div>
 <section>
     <div class="container">
-        <div class="col-sm-5 cent">
+        <div class="col-sm-6 cent">
           <div class="row text-left">
             <div class="card card-light form-card col-12">
             <h4><i class="fa fa-{{ $icon }}" aria-hidden="true"></i> {{ $title }}</h4>

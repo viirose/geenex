197ca8506856a5431b9757a51c36210ee7941cb7
kris <kris@viirose.com>
2019-05-15 15:23:33 +0800

    init
    init

--- resources/views/mails/delete.blade.php
+++ resources/views/mails/delete.blade.php
@@ -3,7 +3,7 @@
 </div>
 
 <div>
-For protect your personal infomations, we have deleted your account with the email : {{ $usr->email }} on GENNEX, You can reregister on joclift.com, anytime!
+For protect your personal infomations, we have deleted your account with the email : {{ $user->email }} on GENNEX, You can reregister on joclift.com, anytime!
 <br>
 Best Regards<br>
 GENNEX Sales Team<br>

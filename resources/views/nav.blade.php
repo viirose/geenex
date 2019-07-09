331894943aba13620c76c50dfa39b67319b0f966
kris <kris@viirose.com>
2019-07-09 13:12:00 +0800

    init
    init

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -36,7 +36,8 @@
   <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container"><a href="./" class="nav-logo"><img src="img/logo.svg" alt="" class="img-fluid"></a>
       <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right">Menu<i class="fa fa-bars ml-2"></i></button>
-      <div id="navbarSupportedContent" class="collapse navbar-collapse">
+      
+      <div id="navbarSupportedContent" class="collapse navbar-collapse pull-left">
         <ul class="navbar-nav ml-auto">
               <!-- Link-->
               <li class="nav-item"> <a href="/" class="nav-link">首页</a></li>

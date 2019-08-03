b254a19323caed8643ffeb976da9126b03e2a79d
kris <kris@viirose.com>
2019-08-03 20:58:10 +0800

    init
    init

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,11 +10,13 @@
       <div class="container text-left">
         <div><h4><a href="/">首页</a>/<a href="/articles">文章中心</a></h4></div>
         <p></p>
+        <p></p>
         <div>
           @if(isset($record))
           <strong>
             <span class="text-primary"><i class="fa fa-line-chart" aria-hidden="true"></i> </span>
             {{ $record->title }}
+            <p></p>
           </strong>
           <div class="list-group">
             <pre>{{ $record->content }}</pre>

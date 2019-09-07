1b4be87f2f9ee24a0527f513f60518cf0f499f3f
kris <kris@viirose.com>
2019-09-07 08:48:45 +0800

    init
    init

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -422,16 +422,16 @@ class ProductController extends Controller
         $image = Image::make($img)
                 ->insert('img/watermark.png')
 
-                ->text('No. '.strtoupper($exists->part_no), 20, 20, function($font) {
+                ->text('No. '.strtoupper($exists->part_no), 20, 40, function($font) {
                     $font->file('fonts/helvetica-light.otf');
-                    $font->size(20);
+                    $font->size(22);
                     $font->color('#fff');
                     $font->align('center');
                     $font->valign('top');
                 })
-                ->text('No. '.strtoupper($exists->part_no), 19, 19, function($font) {
+                ->text('No. '.strtoupper($exists->part_no), 19, 39, function($font) {
                     $font->file('fonts/helvetica-light.otf');
-                    $font->size(20);
+                    $font->size(22);
                     $font->color('#000');
                     $font->align('center');
                     $font->valign('top');

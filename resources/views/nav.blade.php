1412289c6cd92856424b08c3031cac54aa833cee
Kris <31840452+krisnieh@users.noreply.github.com>
2020-07-20 15:07:46 +0800

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -6,7 +6,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>GEENEX</title>
+    <title>GENNEX</title>
     <meta name="description" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
